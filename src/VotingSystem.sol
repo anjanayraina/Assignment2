4f4dac5dbd0412fe90d00733f4faefe137fdd593
anjanayraina <anjanayraina326@gmail.com>
2024-01-16 14:16:45 +0530

    VotingSystem::addCandidate() added

--- src/VotingSystem.sol
+++ src/VotingSystem.sol
@@ -25,6 +25,11 @@ contract VotingSystem is Ownable {
         address voterAddress;
         bool hasVoted;
     }
+
+    /**
+     * @notice Array of candidates.
+     */
+    Candidate[] public candidates;
     /**
      * @notice Map of registered voters.
      */
@@ -32,6 +37,7 @@ contract VotingSystem is Ownable {
     mapping(address => Voter) public voters;
 
     error VoterAlreadyRegistered(address);
+    error ZeroAddressNotAllowed();
     /**
      * @notice Event emitted when a voter registers.
      */
@@ -46,12 +52,25 @@ contract VotingSystem is Ownable {
     /**
      * @notice Function to register a voter. Only the owner can call this function.
      */
-    function registerVoter() public {
+    function registerVoter(string calldata _name) public {
         address _voter = msg.sender;
         if (voters[_voter].voterAddress != address(0)) {
             revert VoterAlreadyRegistered(_voter);
         }
+        voters[_voter] = Voter(_name, _voter, false);
         emit VoterRegistered(_voter);
+    }
+
+    /**
+     * @notice Function to add a candidate. Only the owner can call this function.
+     * @param _name The name of the candidate to add.
+     */
+    function addCandidate(string calldata _name, address _candidateAddress) public onlyOwner {
+        if (_candidateAddress == address(0)) {
+            revert ZeroAddressNotAllowed();
+        }
+        candidates.push(Candidate(_name, _candidateAddress, 0));
+        emit CandidateAdded(_name);
     }
 
     constructor(address _owner) Ownable(_owner) {}
